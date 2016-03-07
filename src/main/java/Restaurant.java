42f50e1c5ee6ce6a5adc1680cae65fdef1acb74a
vrdlc <virgilioramon.delacruz@gmail.com>
2016-03-07 11:32:37 -0800

    added save method with passing test

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -7,6 +7,7 @@ public class Restaurant {
   private String phone;
   private String street;
   private String city;
+  private String state;
   private String zip;
   private int line_average;
   //********ADD NEW CATS HERE**************
@@ -31,6 +32,10 @@ public class Restaurant {
     return city;
   }
 
+  public String getState() {
+    return state;
+  }
+
   public String getZip() {
     return zip;
   }
@@ -41,13 +46,19 @@ public class Restaurant {
 
   //********ADD NEW GETS HERE**************
 
-  public Restaurant(String restaurant_name) {
+  public Restaurant(String restaurant_name, String phone, String street, String city, String state, String zip, int line_average) {
     this.restaurant_name = restaurant_name;
+    this.phone = phone;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.line_average = line_average;
       //********ADD NEW CATS HERE**************
   }
 
   public static List<Restaurant> all() {
-    String sql = "SELECT id, restaurant_name, user_id FROM restaurants ORDER BY restaurant_name";  //********ADD NEW CATS HERE**************
+    String sql = "SELECT id, restaurant_name, phone, street, city, state, zip, line_average FROM restaurants ORDER BY restaurant_name";  //********ADD NEW CATS HERE**************
     try(Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Restaurant.class);
     }
@@ -63,6 +74,7 @@ public class Restaurant {
              this.getPhone().equals(newRestaurant.getPhone()) &&
              this.getStreet().equals(newRestaurant.getStreet()) &&
              this.getCity().equals(newRestaurant.getCity()) &&
+             this.getState().equals(newRestaurant.getState()) &&
              this.getZip().equals(newRestaurant.getZip()) &&
              this.getLineAverage() == (newRestaurant.getLineAverage());
              //********ADD NEW CATS HERE**************
@@ -72,13 +84,14 @@ public class Restaurant {
 //CREATE
 
   public void save() {
-    String sql = "INSERT INTO restaurants (restaurant_name, phone, street, city, zip, line_average) VALUES (:restaurant_name, :phone, :street, :city, :zip, :line_average)";
+    String sql = "INSERT INTO restaurants (restaurant_name, phone, street, city, state, zip, line_average) VALUES (:restaurant_name, :phone, :street, :city, :state, :zip, :line_average)";
     try(Connection con = DB.sql2o.open()) {
       this.id = (int) con.createQuery(sql, true)
           .addParameter("restaurant_name", restaurant_name)
           .addParameter("phone", phone)
           .addParameter("street", street)
           .addParameter("city", city)
+          .addParameter("state", state)
           .addParameter("zip", zip)
           .addParameter("line_average", line_average)
           .executeUpdate()
