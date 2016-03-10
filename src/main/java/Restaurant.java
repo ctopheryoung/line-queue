050dbac82adc634931a0d5755db9b0a9f2f0439a
vrdlc <virgilioramon.delacruz@gmail.com>
2016-03-10 13:44:27 -0800

    trying to merge other branches

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -204,27 +204,27 @@ public class Restaurant {
           .executeScalar(Integer.class);
     }
   }
-  //
-  // public Timestamp getTime() {
-  //   String sql = "SELECT modified FROM check_ins WHERE id = :id";
-  //   try(Connection con = DB.sql2o.open()) {
-  //     return con.createQuery(sql)
-  //       .addParameter("id", id)
-  //       .executeScalar(Timestamp.class);
-  //
-  //   } THIS WORKS!
-  //
-  // }
-
+  
   public Timestamp getTime() {
     String sql = "SELECT modified FROM check_ins WHERE id = :id";
     try(Connection con = DB.sql2o.open()) {
-      Timestamp timestamp = con.createQuery(sql)
+      return con.createQuery(sql)
         .addParameter("id", id)
         .executeScalar(Timestamp.class);
-        //PUT IN SOMETHING THAT CHANGES TIMESTAMP TO STRING IN HERE?
-    }
+
+    } THIS WORKS!
+
   }
+
+  // public Timestamp getTime() {
+  //   String sql = "SELECT modified FROM check_ins WHERE id = :id";
+  //   try(Connection con = DB.sql2o.open()) {
+  //     Timestamp timestamp = con.createQuery(sql)
+  //       .addParameter("id", id)
+  //       .executeScalar(Timestamp.class);
+  //       //PUT IN SOMETHING THAT CHANGES TIMESTAMP TO STRING IN HERE?
+  //   }
+  // }
   // public String getTimeStamp() {
   //   String sql = "SELECT modified FROM check_ins WHERE id = :id";
   //   try (Connection con = DB.sql2o.open()) {
